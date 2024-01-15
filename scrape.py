bf12e3f7119603be3c14fde6badee7dbddaf25e4
szRR <sezer.tetik2005@gmail.com>
2024-01-15 22:53:55 +0300

    ☑️ Project updated to solve GitHub workflow issues.

--- scrape.py
+++ scrape.py
@@ -3,18 +3,19 @@ import os.path
 import time
 from pathlib import Path
 
+from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.common.action_chains import ActionChains
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service as ChromeService
-from selenium.webdriver.common.keys import Keys
-from webdriver_manager.chrome import ChromeDriverManager
+import chromedriver_autoinstaller
+
 
 def scrape_colors_and_save_data_as_json() -> int:
-    driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+    chromedriver_autoinstaller.install()
+    driver = webdriver.Chrome()
     driver.get("https://coolors.co/colors")
     delay = 2.5
 
